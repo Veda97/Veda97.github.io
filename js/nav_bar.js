314570d0830f704e40922fca90cd9bea16351c75
Neethu-nr <neethurenjith0@gmail.com>
2023-12-30 16:32:59 -0800

    working research page basic

---
+++ js/nav_bar.js
@@ -0,0 +1,29 @@
+
+
+function navBar() {
+    const headerContainer = document.createElement('div');
+    headerContainer.innerHTML = `
+    <!-- Navigation -->
+    <nav class="navbar" id="mainNav">
+    <div class="container">
+        <ul class="navbar-items">
+            <li class="nav-item">
+                <a class="nav-link js-scroll-trigger" href="../index.html">Home</a>
+            </li>  
+            <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="../pages/research.html">Research</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="../pages/experience.html/">Experience</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="../pages/outreach">Outreach</a>
+            </li>
+        </ul>
+        </div>
+    </div>
+    </nav>
+    `;
+
+    document.body.prepend(headerContainer);
+}
